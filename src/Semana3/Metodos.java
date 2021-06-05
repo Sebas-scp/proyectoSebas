2d97619ae5439aa703aeb8b75eecb9381330d624
usuario <usuario@pc>
2021-06-04 19:48:49 -0600

    Clase 3, hablamos de metodos y como invocarlos

---
+++ src/Semana3/Metodos.java
@@ -0,0 +1,66 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Semana3;
+
+/**
+ *
+ * @author usuario
+ */
+public class Metodos 
+{
+   
+   
+   //este metodo suma dos numeros tipo entero
+   public static int pedro(int numero1, int numero2)
+   {
+      int resultado;
+      resultado = numero1 + numero2;
+      return resultado;
+    }
+   
+    //este metodo suma dos numeros de tipo float
+    public static float pedro(float numero1, float numero2)
+   {
+      float resultado;
+      resultado = numero1 + numero2;
+      return resultado;
+    }
+    
+    /**
+     *el metodo maria multiplica dos numeros
+     * esto se saca tocando el bombillo de missing javadoc
+     * @param numero1
+     * @param numero2
+     * @return
+     */
+    public static int multiplicar(int numero1, int numero2)
+   {
+      int resultado;
+      resultado = numero1 * numero2;
+      return resultado;
+    }
+   
+   public static int multiplicar(int numero1, int numero2, int numero3)
+   {
+      int resultado;
+      resultado = numero1 * numero2 * numero3;
+      return resultado;
+    }
+   
+   
+   public static void main (String[] args) 
+   {
+       //creamos un espacio donde se almacena lo que me da pedro
+       int resultadoParaJefe;
+       //digito el nombre del metodo (valores de lo que queremos sumar)
+      resultadoParaJefe =  pedro(3,4);
+      System.out.print(resultadoParaJefe);
+       
+   }
+}     
+
+           
+
