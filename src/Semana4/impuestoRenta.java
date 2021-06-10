b4321b4fd175f7932f3e19b436d052d769b99911
usuario <usuario@pc>
2021-06-09 22:41:06 -0600

    ejemplos de semana 4, usos do while, for, uso de contadores y while tambien, tarea problema 2, impuesto renta

---
+++ src/Semana4/impuestoRenta.java
@@ -0,0 +1,80 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Semana4;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author usuario
+ */
+public class impuestoRenta 
+{
+ public static double calcularImpuesto(int numDependientes, double salario)
+    {
+        double monto;
+        if (salario < 200000)
+            monto = salario;
+        else if ( salario < 400000 )
+            monto = 0.10;
+        else if ( salario < 600000 )
+            monto = 0.15; 
+        else if ( salario < 800000 )
+            monto = 0.20;    
+        else if ( salario > 800000 )
+            monto = 0.30;
+        
+        
+        double monto2;
+        if (numDependientes == 1)
+            monto2 = salario*1;
+        else if ( numDependientes == 2 )
+            monto2 = salario*0.90;
+        else if ( numDependientes == 3 )
+            monto2 = salario*0.70;
+        else if ( numDependientes == 4 )
+            monto2 = salario*0.60;
+        else if ( numDependientes >4 )
+            monto2 = salario*0.40;
+        
+        double impuesto = monto2*monto;
+        return impuesto;
+    }
+ 
+ 
+  public static void main(String[] args)
+  {
+    Scanner entrada = new Scanner(System.in);
+    
+    System.out.print("Introduzca su salario: ");
+    double salario= entrada.nextDouble();
+    
+    System.out.println("Introduzca sus dependientes: ");
+    int numDependientes = entrada.nextInt();
+    
+    double pagar = calcularImpuesto(numDependientes,salario);
+    System.out.printf("El monto a pagar es:  %f",pagar);
+    
+    
+  }
+ 
+
+
+
+  
+
+
+
+
+
+
+
+
+
+
+
+    
+}
