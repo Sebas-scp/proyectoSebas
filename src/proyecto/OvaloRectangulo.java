ede0a909d508ac2e35d559f3f53a4e4beefdb0b3
usuario <usuario@pc>
2021-08-06 14:06:23 -0600

    se realizo el ejercicio 5

---
+++ src/proyecto/OvaloRectangulo.java
@@ -0,0 +1,14 @@
+
+package proyecto;
+
+/**
+ * @author Sebastian Cordero Mora
+ */
+public class OvaloRectangulo 
+{
+    JPanelDibujo dibujo= new JPanelDibujo();
+    
+    Ovalo figura1 = new Ovalo((int) Math.random()*100,(int) Math.random()*100,(int) Math.random()*100,(int) Math.random()*100);
+    Rectangulo figura2 = new Rectangulo((int) Math.random()*100,(int) Math.random()*100,(int) Math.random()*100,(int) Math.random()*100);
+}
+
